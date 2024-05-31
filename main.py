e24dfd7c60fd44e8b9f7f39dc4ac8bbb43ee7c5b
Gerson Gabriel de Oliveira Da Silva <47606708827@hmmg.local>
2024-05-31 15:53:11 -0300

    Graham method

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 # import pandas as pd
 import streamlit as st
-# import time
+import time
 
 from Classes.StatusInvest import statusInv
 
+
 def main():
     
-    statusInv.setaAcoes(['bbas3', 'bbse3'])
+    statusInv.setaAcoes(['bbas3', 'bbse3', 'fesa4', 'cmig4', 'neoe3', 'klbn11', 'taee11', 'itsa4','vale3','cxse3'])
     indicadores = statusInv.GetIndicadores()
+    # st.write(indicadores)
     graham_method = statusInv.getGrahamMethod(indicadores)
-   
-    st.write(graham_method)
+  
+ 
+
+    st.title("Graham method")
+    st.dataframe(graham_method, hide_index=True)
+    # st.table(graham_method)
     
     
     # stausInv.getValues()
@@ -18,8 +24,8 @@ def main():
  
 
 if __name__ == "__main__":
-    #  while True:
-    #     main()
-    #     time.sleep(5*60)
-    #     st.rerun()
-    main()
+    while True:
+        main()
+        time.sleep(5*60)
+        st.rerun()
+    # main()
